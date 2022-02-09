1dcb51a62b85ee556a7e552fb9155c988c8ff4af
donevska <angeladonevska@gmail.com>
2022-02-10 00:57:57 +0100

    Made some CSS changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,33 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <img
+          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/026/921/original/Data-Dictionary-vs-Business-Glossary-Landing-Page-1710x495-1.png?1644449969"
+          alt="banner"
+        ></img>
+        <header className="App-header">Dictionary</header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Angela Donevska</small>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/angela-donevska"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Angela Donevska
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/donevska/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </small>
         </footer>
       </div>
     </div>
