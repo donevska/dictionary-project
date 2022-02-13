6e6a64d665cb9e889dc9d79b467f571888f47a28
donevska <angeladonevska@gmail.com>
2022-02-13 21:13:51 +0100

    CSS changes added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
-        <img
-          className="HeaderImage"
-          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/026/921/original/Data-Dictionary-vs-Business-Glossary-Landing-Page-1710x495-1.png?1644449969"
-          alt="banner"
-        ></img>
+        <section>
+          <header className="App-header">Dictionary</header>
+          <img
+            className="HeaderImage"
+            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/026/921/original/Data-Dictionary-vs-Business-Glossary-Landing-Page-1710x495-1.png?1644449969"
+            alt="banner"
+          ></img>
+        </section>
         <main>
           <Dictionary defaultKeyword="ocean" />
         </main>
